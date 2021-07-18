206c6771565fcb99e4933268b01f808ac626d2ad
Leonardo <leomadeiraa@hotmail.com>
2021-07-17 23:29:38 -0300

    Create condicoes.php

---
+++ condicoes.php
@@ -0,0 +1,13 @@
+<?php
+
+$x = 5;
+$y = 10;
+$i = -3;
+
+if ($x >= $y) { //if = SE
+  echo 'x é maior que y?';
+} else if ($i >= $y) { // else if = Senão se
+  echo 'i é maior que y?';
+} else { // else = senão
+  echo 'x não é maior que y e i não é maior que y';
+}
