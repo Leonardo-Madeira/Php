5b451f4deb7146a91a7d02b103a76ee0e4917c1b
Leonardo <leomadeiraa@hotmail.com>
2021-07-24 23:56:09 -0300

    Create heranca.php

---
+++ fundamentos/POO/heranca.php
@@ -0,0 +1,20 @@
+<?php
+
+class Aplicacao
+{
+  public function runApp()
+  {
+    echo 'rodando!';
+  }
+}
+
+class Cliente extends Aplicacao
+{
+  public static function nomeCliente()
+  {
+    echo 'Leonardo Madeira';
+  }
+}
+
+$cliente = new Cliente();
+$cliente->runApp();
