357069116e97a33815e4d4ddc6f5cab007208244
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-07-26 23:57:47 -0300

    Update concatenacao.php

--- fundamentos/concatenacao.php
+++ fundamentos/concatenacao.php
@@ -5,5 +5,4 @@ $idade = 45;
 $n1 = '<br>'; // posso fazer desta forma tbm
 
 echo 'ex1: Olá, ', $cliente, '. Tudo bem com você? Estou vendo que você tem ', $idade, ' anos. <br><br>'; // essa é uma forma não tao interessante.
-
 echo "ex2: Olá, $cliente!"; // essa é a forma mais interessante e simples :)
