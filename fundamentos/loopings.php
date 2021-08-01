6e7e47477ebd218adc7f1e18080333e6924b6355
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-07-31 23:50:20 -0300

    Update loopings.php

--- fundamentos/loopings.php
+++ fundamentos/loopings.php
@@ -1,7 +1,7 @@
 <?php
 //FOR
 echo 'FOR: <br>';
-for ($i = 1; $i <= 10; $i++) {
+for ($i = 1; $i <= 10; $i++){
   echo "$i: Olá, FOR <br>";
 }
 //FIM FOR
@@ -10,7 +10,7 @@ for ($i = 1; $i <= 10; $i++) {
 $a = 1;
 echo '<br>';
 echo 'WHILE: <br>';
-while ($a <= 10) {
+while ($a <= 10){
   echo "<hr >$a: Olá, while ";
   $a++;
 }
@@ -22,7 +22,7 @@ echo '<br>';
 echo '<br>';
 echo 'DO WHILE: <br>';
 $b = 1;
-do {
+do{
   echo "<br >$b: Olá, do while ";
   $b++;
 } while ($b <= 10);
