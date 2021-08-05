8fbd68694bb50048f649929ca7a0ab1f37294e7a
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-08-04 23:15:47 -0300

    Update foreach.php

--- fundamentos/foreach.php
+++ fundamentos/foreach.php
@@ -3,10 +3,12 @@
 $clientes = ['cliente1', 'cliente2', 'cliente3', 'cliente4', 'cliente5'];
 
 foreach ($clientes as $key => $value) {
+  
   echo  "$key => $value <hr>";
-  //key é a quantidade de variaveis que estão dentro da array clientes, que no caso são 5.
 
-  //value é o valor(elemento) do array, que no caso é o nome dos clientes.
+    //key é a quantidade de variaveis que estão dentro da array clientes, que no caso são 5.
+
+    //value é o valor(elemento) do array, que no caso é o nome dos clientes.
 }
 
 // OU
