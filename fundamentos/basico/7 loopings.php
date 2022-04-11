091dd03c6427beaf38bcdfd3879790b5754b85fc
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-04-10 21:50:25 -0300

    Create 7 loopings.php

---
+++ fundamentos/basico/7 loopings.php
@@ -0,0 +1,29 @@
+<?php
+//FOR
+echo 'FOR: <br>';
+for ($i = 1; $i <= 10; $i++){
+  echo "$i: Olá, FOR <br>";
+}
+//FIM FOR
+
+//WHILE
+$a = 1;
+echo '<br>';
+echo 'WHILE: <br>';
+while ($a <= 10){
+  echo "<hr >$a: Olá, while ";
+  $a++;
+}
+//FIM WHILE
+
+
+//DO WHILE
+echo '<br>';
+echo '<br>';
+echo 'DO WHILE: <br>';
+$b = 1;
+do{
+  echo "<br >$b: Olá, do while ";
+  $b++;
+} while ($b <= 10);
+  //FIM DO WHILE
