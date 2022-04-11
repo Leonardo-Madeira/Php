3c2085f33651203571d578aa65b18c9479a89237
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-04-10 21:50:29 -0300

    Create 2 arraysMultidimensionais.php

---
+++ fundamentos/Intermediário/2 arraysMultidimensionais.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $arr = array(array('Leonardo','Madeira'),array(25, 35));
+        echo $arr[0][0];
+
+    ?>
+</body>
+</html>
