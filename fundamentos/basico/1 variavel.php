f6331d883a8268960eb3f4e64847432c5c54cd7d
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-04-10 21:50:12 -0300

    Create 1 variavel.php

---
+++ fundamentos/basico/1 variavel.php
@@ -0,0 +1,18 @@
+<?php
+
+//variaveis
+$nome = 'Léo'; // string / caractere
+$idade = 24; // inteiro
+$real = 50.50 // real / float
+$boolean = true; // logico -> retorna 1, tudo o que é true sempre retornará 1
+$boolean2 = false; // false retorna sempre Vazio
+
+
+//Constante
+define('user', 'Leonardo');
+echo user;
+
+
+// Podemos utilizar tags de HTML dentro do echo
+echo '<h1>Olá, mundo</h1>'
+echo '<p>Lorem ipsum</p>'
