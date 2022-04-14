ead1d2a1190487e27e0061c94179b25f258231b8
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-04-14 15:48:28 -0300

    Create 6 functionsStrings.php

---
+++ fundamentos/Intermediário/6 functionsStrings.php
@@ -0,0 +1,13 @@
+<?php
+
+    $conteudo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus saepe eum delectus voluptatibus
+    quos est repellendus, sunt optio ipsum nostrum a ratione aperiam quibusdam dolorum at aliquam laudantium sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus saepe eum delectus voluptatibus
+    quos est repellendus, sunt optio ipsum nostrum a ratione aperiam quibusdam dolorum at aliquam laudantium sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus saepe eum delectus voluptatibus
+    quos est repellendus, sunt optio ipsum nostrum a ratione aperiam quibusdam dolorum at aliquam laudantium sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus saepe eum delectus voluptatibus
+    quos est repellendus, sunt optio ipsum nostrum a ratione aperiam quibusdam dolorum at aliquam laudantium sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus saepe eum delectus voluptatibus
+    quos est repellendus, sunt optio ipsum nostrum a ratione aperiam quibusdam dolorum at aliquam laudantium sed.Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus saepe eum delectus voluptatibus
+    quos est repellendus, sunt optio ipsum nostrum a ratione aperiam quibusdam dolorum at aliquam laudantium sed.";
+
+    echo substr($conteudo,0,100); echo '...';
+    
+?>
