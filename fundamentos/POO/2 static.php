b4eb1acfb9aabf55ab1b203d984b794edb9d32f2
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-04-14 15:47:29 -0300

    Create 2 static.php

---
+++ fundamentos/POO/2 static.php
@@ -0,0 +1,12 @@
+<?php   // public static
+class Nome{
+  public static $nomeCompleto = 'Leonardo Madeira';
+  public static function printNome(){
+    echo 'Leonardo';
+  }
+}
+
+//Instanciar a class.
+Nome::printNome();
+Nome::$nomeCompleto; // desta forma, a variavel statica não é instanciada e nao contem erro
+echo self::$nomeCompleto;
