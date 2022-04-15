0bfcd59a793f471715b952d15242cbb71e4646c6
Leonardo Madeira <leomadeiraa@hotmail.com>
2022-04-15 11:07:01 -0300

    Create 7 switch.php

---
+++ fundamentos/Intermediário/7 switch.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nome = 'Unknown';
+    switch ($nome) {
+        case 'Leonardo':
+            echo "Variavel é $nome";
+            break;
+        case 'Madeira':
+            echo "Variavel é $nome";
+            break;
+        case 'Unknown':
+            echo "Variavel é $nome";
+            break;
+        default:
+            echo "Nada corresponde aos requisitos";
+            break;
+    }
+
+?>
